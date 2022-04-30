a50703b22b1fb1f4660838bfd26a9ac9ced5cab4
serawrT <96268768+serawrT@users.noreply.github.com>
2022-04-29 19:27:20 -0700

    added data base model

--- back_end.py
+++ back_end.py
@@ -1,9 +1,28 @@
 from crypt import methods
 from trace import Trace
 from turtle import back
-from flask import Flask, render_template, request
+from unicodedata import category
+from flask import Flask, render_template, request, redirect
+import flask_sqlalchemy import SQLAlchemy
+import os
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(
+    os.path.join(project_dir, "mydatabase.db")
+)
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+db = SQLAlchemy(app)
+
+
+class Expense(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50), nullable=False)
+    expensename = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
 
 
 @back_end.route('/')
@@ -16,8 +35,8 @@ def addexpense():
     date = request.form["date"]
     expensename = request.form['expensename']
     amount = request.form["amount"]
-    catagory = request.form["category"]
-    print(date + " " + expensename + " "amount + " " + category)
+    category = request.form["category"]
+    print(date + " " + expensename + " " + amount + " " + category)
     return redirect(("/"))
 
 
